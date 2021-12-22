7eeb942d7d62f5c418b2125c216335f67fe4a18c
Martin Valchev <mv1u18@soton.ac.uk>
2021-12-23 00:07:57 +0200

    style(layout): more consistent paddings/margins

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ import StickyNote from '../components/StickyNote';
 
 const Home: NextPage = () => {
   return (
-    <div className='h-screen max-w-4xl mx-auto flex items-center flex-col pt-10'>
+    <div className='h-full max-w-4xl mx-auto flex items-center flex-col py-10 gap-10'>
       <Seo />
       <h1 className='text-4xl'>Fuckup Wall</h1>
-      <div className='w-full m-10 grid grid-cols-[repeat(auto-fit,_minmax(13rem,_max-content))] justify-center gap-5'>
+      <div className='w-full grid grid-cols-[repeat(auto-fit,_minmax(13rem,_max-content))] justify-center gap-5'>
         {mock.map((e, idx) => (
           <StickyNote key={idx} note={e} />
         ))}
